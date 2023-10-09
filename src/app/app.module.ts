3c6e509252fbfa2ff84c8faa0ebc63826a963301
Oleg <olegpl1993@gmail.com>
2023-10-10 02:02:47 +0300

    feat: layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoComponent } from './logo/logo.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
