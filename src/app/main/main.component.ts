361ed81d7c1b989b93ad9f61157dadc67f27a5e6
Oleg <olegpl1993@gmail.com>
2023-10-11 03:19:23 +0300

    feat: test render ngFor

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [CommonModule],
 })
 export class MainComponent {
+  public name = 'MainComponent';
+  public data = [
+    {
+      title: 'Матрица',
+      description: 'Киано тащит весь фильм на себе',
+    },
+    {
+      title: 'Хотабыч',
+      description: 'Хороший фильм про деда волшебника',
+    },
+    {
+      title: 'Аладин',
+      description: 'Фильм про джина из лампы',
+    },
+  ];
 
+  public say(value: string) {
+    console.log(value);
+  }
+  public sayName() {
+    console.log(this.name);
+  }
 }
